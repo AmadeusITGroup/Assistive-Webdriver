d0280a5b7c313a761197f690d9e32b9dd00768f8
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2020-03-02 14:46:53 +0100

    Adding GitHub workflows to build projects

--- text-to-socket-engine/build.cmd
+++ text-to-socket-engine/build.cmd
@@ -1,5 +1,5 @@
 @echo off
 midl /h TextToSocketEngine.h TextToSocketEngine.idl
 rc resource.rc
-cvtres /MACHINE:X86 /OUT:resource.obj resource.res
-cl resource.obj *.cpp /LD /link /MACHINE:X86 /DEF:TextToSocketEngine.def /OUT:TextToSocketEngine.dll
+cvtres /OUT:resource.obj resource.res
+cl resource.obj *.cpp /LD /link /DEF:TextToSocketEngine.def /OUT:TextToSocketEngine.dll
