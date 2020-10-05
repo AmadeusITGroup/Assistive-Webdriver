1f75406fc6c6ac3850da049d4d4168de00825220
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2020-10-05 16:30:33 +0200

    Updating chromium and nvda in msedge-win10 vagrant vm

--- vagrant/msedge-win10/download.sh
+++ vagrant/msedge-win10/download.sh
@@ -35,12 +35,13 @@ function downloadFile() {
 }
 
 downloadFile MSEdge.Win10.Vagrant.zip https://az792536.vo.msecnd.net/vms/VMBuild_20190311/Vagrant/MSEdge/MSEdge.Win10.Vagrant.zip 10620d68a5a14129da0bc7f856e0e3cabbfcec9cb8e6464553c03d2f638978f9b98866cf405baf737073ebb72eee904e8cb15d835ff5f12c99b0b49d668b531a
-downloadFile nvda.exe https://www.nvaccess.org/files/nvda/releases/2019.3.1/nvda_2019.3.1.exe d2b4cde3229aed0b8cf3cf4a6d76b4e017d0101f469d358696417edaad002f0c988315be4910021114e11fdd07956c8e94137ee4a01cda9db02d8613add268e5
+downloadFile nvda.exe https://www.nvaccess.org/files/nvda/releases/2020.2/nvda_2020.2.exe c759b054e4600e5de6a27a576fe0b3039f37bd4839ee4d2d63787950eda0f8d5b0f18f9ede20f94e498f5f0c2fb92e0304a25189445a085d4b65e48de3b72169
 downloadFile openjdk.zip https://download.java.net/java/GA/jdk14.0.1/664493ef4a6946b186ff29eb326336a2/7/GPL/openjdk-14.0.1_windows-x64_bin.zip 7546b40977670f24ec68ad82b5d9f52f4fc105410e7bbfec6da0718795f4a82ac26665f1f9f05562ae20710aefe85c8dac05c5345d4e48385e99323a916d2157
 downloadFile selenium-server.jar https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar 29ae1e11e51744478e55ba12f6d0dbc06ce7850886701e84d97bb44a675af5f0d8c5bb7b8278365718657e9cec34a69b390cc7568dc2fe710c3256a1effed2ac
 downloadFile node.zip https://nodejs.org/dist/v12.16.3/node-v12.16.3-win-x64.zip 3cee3f6d691a08e5e665b9ff92e7fa242d237abb138bfd88975641f9e1931a58895a8130e615e1cb54ddc8d819a783e2e937766ce7fa3ab9bf36890aa787f415
-downloadFile chrome.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Win_x64%2F737173%2Fchrome-win.zip?generation=1580439619624464&alt=media' 4eac4f1e7e7f12978114acb174da0ea8aece7eab839856c17f038a5b8bb30673ceccdf465517983c835089f35949128a40ed683f2af316596253bc9a26b325f1
-downloadFile chromedriver.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Win_x64%2F737173%2Fchromedriver_win32.zip?generation=1580439625877582&alt=media' b608696ba1782bd4b285a7f3c13fd0969d191270308ec240835fe08e82c598e05469f27fa2c2421136d88f9f13bdf274708ef340d11862ef34ec83f5d59baed5
+# cf https://www.chromium.org/getting-involved/download-chromium
+downloadFile chrome.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Win%2F782792%2Fchrome-win.zip?generation=1593136034368251&alt=media' 39c1965233b995828235cfbcb0d1b4df757a6c94cf0df43cb58de8dc75006a5752f107ebe4ad4d2a7ac16f7bc000e8e4dcf2cb7c645e9697d6838f512814ddda
+downloadFile chromedriver.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Win%2F782792%2Fchromedriver_win32.zip?generation=1593136040882187&alt=media' a23c1e33ea5c9e02cb9c813d44a2e05e441eb7c88a8c8db0ec7b1c5fb4029ddd37d80683bc53c1de61f0559d30594dcba3dffd0dcebe0ee817314f7854d0c319
 downloadFile text-to-socket-engine-x86.zip https://github.com/AmadeusITGroup/Assistive-Webdriver/releases/download/text-to-socket-engine%2F0.0.1/text-to-socket-engine-x86.zip f0007140925a5640d4d34fa0d04f90db094d5e31871d42ffeada817258d72e63dbdb55044a035aa3aa372e7efa8ed5ab194f7af45580bd2399174d347b9481d1
 downloadFile tcp-web-listener.tgz https://registry.yarnpkg.com/tcp-web-listener/-/tcp-web-listener-0.0.1.tgz 41b6e700dbadf5314a61b2837dba04965eb806ac39f379e1b191eaa696cf0001201c8a0e28768ae04caa592bfc5e7f2ffef21e0f674aaccda2f22ba9f4c89abd
 
