8212a83b0fb4503c69a9a9722c40b1ae1f57ca59
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2021-02-24 14:31:32 +0100

    Using nick-invision/retry@v2 instead of duplicating tasks in ci.yml

--- vagrant/win10-chromium-nvda/createVM.sh
+++ vagrant/win10-chromium-nvda/createVM.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-vagrant up &&
-VBoxManage controlvm win10-chromium-nvda setvideomodehint 1280 720 32 &&
-vagrant snapshot save nvda &&
+set -e
+
+if [ "$1" == "--destroy-existing" ] ; then
+  killall vagrant ruby || true
+  vagrant destroy -f
+fi
+
+vagrant up
+VBoxManage controlvm win10-chromium-nvda setvideomodehint 1280 720 32
+vagrant snapshot save nvda
 vagrant halt
