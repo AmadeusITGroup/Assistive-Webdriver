c03057d47d9badab2ddb730280de8d2f0bdb937f
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2021-09-13 15:55:31 +0200

    Updating nodejs version

--- vagrant/win10-chromium-nvda/start.cmd
+++ vagrant/win10-chromium-nvda/start.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-start "assistive-playwright-server" /min "c:\node-v12.16.3-win-x64\node_modules\win10-chromium-nvda-vm\node_modules\.bin\assistive-playwright-server.cmd"
+start "assistive-playwright-server" /min "c:\node-v14.17.6-win-x64\node_modules\win10-chromium-nvda-vm\node_modules\.bin\assistive-playwright-server.cmd"
 start "selenium-server" /min "c:\jdk-14.0.1\bin\java.exe" -jar "c:\selenium-server.jar"
 start "nvda" /min "c:\Program Files (x86)\NVDA\nvda.exe"
