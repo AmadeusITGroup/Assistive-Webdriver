c33a343b1145e1699ca11023f37a50cdd2a35f44
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2021-09-14 18:06:37 +0200

    Adding Jaws and tcp-web-listener to the vagrant vm

--- vagrant/win10-chromium-nvda/createVM.sh
+++ vagrant/win10-chromium-nvda/createVM.sh
@@ -3,11 +3,47 @@
 set -e
 
 if [ "$1" == "--destroy-existing" ] ; then
-  killall vagrant ruby || true
+  echo "Destroying existing VM"
   vagrant destroy -f
 fi
 
-vagrant up
-VBoxManage controlvm win10-chromium-nvda setvideomodehint 1280 720 32
-vagrant snapshot save nvda
-vagrant halt
+if vagrant snapshot list | grep root &> /dev/null ; then
+  echo "Snapshot root already exists"
+else
+  echo "Creating VM from scratch"
+  vagrant destroy -f
+  vagrant up
+  vagrant halt
+  vagrant snapshot save root
+fi
+if vagrant snapshot list | grep noscreenreader &> /dev/null ; then
+  echo "Snapshot noscreenreader already exists"
+else
+  echo "Creating noscreenreader snapshot"
+  vagrant halt -f
+  vagrant snapshot restore --no-start root
+  vagrant up --provision-with start-assistive-playwright,start-selenium-webdriver
+  VBoxManage controlvm win10-chromium-nvda setvideomodehint 1920 1080 32
+  vagrant snapshot save noscreenreader
+  vagrant halt -f
+fi
+if vagrant snapshot list | grep nvda &> /dev/null ; then
+  echo "Snapshot nvda already exists"
+else
+  echo "Creating nvda snapshot"
+  vagrant halt -f
+  vagrant snapshot restore --no-start noscreenreader
+  vagrant up --provision-with start-nvda
+  vagrant snapshot save nvda
+  vagrant halt -f
+fi
+if vagrant snapshot list | grep jaws &> /dev/null ; then
+  echo "Snapshot jaws already exists"
+else
+  echo "Creating jaws snapshot"
+  vagrant halt -f
+  vagrant snapshot restore --no-start noscreenreader
+  vagrant up --provision-with start-jaws
+  vagrant snapshot save jaws
+  vagrant halt -f
+fi
---
+++ vagrant/win10-chromium-nvda/vm-skip-keys.sh
@@ -0,0 +1,2 @@
+export NVDA_SKIP_KEYS="Insert"
+export JAWS_SKIP_KEYS="Insert F3 Backspace ControlLeft ControlRight ShiftLeft ShiftRight Tab Delete End Home PageDown PageUp ArrowDown ArrowLeft ArrowRight ArrowUp Escape Numpad0 Numpad1 Numpad2 Numpad3 Numpad4 Numpad5 Numpad6 Numpad7 Numpad8 Numpad9 NumpadDivide NumpadMultiply NumpadSubtract NumpadAdd NumpadEnter NumpadDecimal"
