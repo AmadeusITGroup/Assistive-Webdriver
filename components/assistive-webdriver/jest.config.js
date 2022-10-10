cd8ab8d03c142342bd38cd28c1cd64cd170cd783
divdavem <david-emmanuel.divernois@amadeus.com>
2022-10-10 11:16:32 +0200

    Fix ts-jest global config warning

--- components/assistive-webdriver/jest.config.js
+++ components/assistive-webdriver/jest.config.js
@@ -18,14 +18,12 @@
 
 module.exports = {
   transform: {
-    "^.+\\.ts$": "../../tools/code-coverage/jestTransform"
+    "^.+\\.ts$": [
+      "../../tools/code-coverage/jestTransform",
+      { tsconfig: "tsconfig.test.json" }
+    ]
   },
   testEnvironment: "../../tools/code-coverage/jestEnv",
   testMatch: ["<rootDir>/test/**/*.spec.ts"],
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.test.json"
-    }
-  },
   testTimeout: 10000
 };
